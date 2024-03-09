7409afb19fcc110c886ecac121b8988a60c6befa
Dean-PC\Dean <dmurphy1994@hotmail.co.uk>
2024-03-09 21:32:27 +0000

    added visualisation for the clustering

--- src/test/java/KMeansTest.java
+++ src/test/java/KMeansTest.java
@@ -1,7 +1,11 @@
 import Helper.ClusterTreeHelper;
 import Model.BestNode;
 import Model.ClusteringNode;
+import View.PanZoomPanel;
+import ViewController.PanZoomController;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -34,14 +38,15 @@ public class KMeansTest {
         return copy;
     }
 
-    public static void assignToCentroids(double[][] data, ClusteringNode centroidTree)
+    public static void assignToCentroids(double[][] data, int[] assignedTo, ClusteringNode centroidTree)
     {
-        for (double[] d: data)
+        for (int i=0;i<data.length;i++)
         {
             BestNode<ClusteringNode> bestNode = new BestNode<ClusteringNode>();
-            centroidTree.nearest(d, bestNode);
+            centroidTree.nearest(data[i], bestNode);
 
-            bestNode.node.addPoint(d);
+            bestNode.node.addPoint(data[i]);
+            assignedTo[i] = bestNode.node.getIndex();
         }
     }
 
@@ -79,20 +84,57 @@ public class KMeansTest {
         return total;
     }
 
-    public static void main(String[] args)
-    {
+    public static void viewPanel(final double[][] data,
+                                 final int[] centroidAssignment, final Color[] centroidColors) throws InterruptedException {
+        JFrame window = new JFrame("Test Window");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.setSize(600,600);
+
+        PanZoomController panZoomController = new PanZoomController();
+        PanZoomPanel view = new PanZoomPanel(panZoomController) {
+
+            @Override
+            protected void renderImplementation(Graphics g) {
+
+                for (int i=0;i<data.length;i++)
+                {
+                    g.setColor(centroidColors[centroidAssignment[i] % centroidColors.length]);
+                    g.fillOval((int)data[i][0], (int)data[i][1], 1, 1);
+                }
+            }
+        };
+
+        window.add(view);
+        window.setVisible(true);
+
+//        panZoomController.pan(new double[]{view.getWidth() / 2, view.getHeight() / 2});
+        panZoomController.zoomOnWorldPosition(new double[]{0, 0}, 50);
+
+        while(true)
+        {
+            view.repaint();
+
+            Thread.sleep(100);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         KMeansTest test = new KMeansTest();
 
         int dimensions = 2, iterations = 0;
         double[][] data = test.createRandomData(100, dimensions, new Random(123));
-        double[][] centroids = test.createRandomData(2, dimensions, new Random());
-        double delta = 0, err = 0.1;
+
+        int[] centroidAssignment = new int[data.length];
+        double[][] centroids = test.createRandomData(3, dimensions, new Random());
+
+        double delta, err = 0.1;
 
         do {
             ClusterTreeHelper treeHelper = new ClusterTreeHelper();
             ClusteringNode centroidTree = treeHelper.createKDTree(cloneData(centroids));
 
-            assignToCentroids(data, centroidTree);
+            assignToCentroids(data, centroidAssignment, centroidTree);
 
             double[][] newCentroids = new double[centroids.length][];
             updateCentroids(newCentroids, centroidTree);
@@ -105,5 +147,8 @@ public class KMeansTest {
 
         System.out.println(Arrays.deepToString(centroids));
         System.out.println(iterations);
+
+        viewPanel(data, centroidAssignment,
+                new Color[]{Color.RED, Color.GREEN, Color.BLUE, Color.BLACK});
     }
 }
